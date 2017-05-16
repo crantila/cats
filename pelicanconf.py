b7c19ef01b31e578b3961be1759bec6700445e10
Christopher Antila <christopher@antila.ca>
2017-05-15 21:20:04 -0400

    Some more settings changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,29 +7,39 @@ SITENAME = 'Meow'
 SITEURL = ''
 
 PATH = 'content'
-
 TIMEZONE = 'America/Toronto'
-
 DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
 
-# Feed generation is usually not desired when developing
+# Disable the feeds
+# =================
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+# Change the URLs
+# ===============
+ARCHIVES_SAVE_AS = ''
+ARTICLES_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+PAGE_URL = '{slug}'
+PAGE_SAVE_AS = '{slug}/index.html'
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+
+# Theme (Pelican Settings)
+# ========================
+THEME_STATIC_DIR = 'static'
+TYPOGRIFY = True
+# TEMPLATE_PAGES = {
+# }
+
+
+# Theme (Custom Settings)
+# =======================
+THEME = 'theme'
